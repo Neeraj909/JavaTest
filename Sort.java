08eb75c4ed1740a270f68759320227dc6bc760a2
neeraj.b.sharma <neeraj.b.sharma@rakute.com>
2023-07-03 13:49:34 +0530

    Merge Two Sort Array

--- Sort.java
+++ Sort.java
@@ -49,10 +49,48 @@ public class Sort {
         }
         System.out.println("Insertion Sort Array "+Arrays.toString(arr));
     }
+
+    public static void mergeTwoSortArray(int[] arr1,int[] arr2){
+        int[] mergeArray=new int[arr1.length+arr2.length];
+        int index=0;
+        int i=0;
+        int j=0;
+        while (i<arr1.length && j<arr2.length){
+            if(arr1[i]<arr2[j]){
+                mergeArray[index]=arr1[i];
+                index++;
+                i++;
+            }
+            else{
+                mergeArray[index]=arr2[j];
+                index++;
+                j++;
+            }
+        }
+        while (i<arr1.length){
+            mergeArray[index]=arr1[i];
+            index++;
+            i++;
+        }
+        while (j<arr2.length){
+            mergeArray[index]=arr2[j];
+            index++;
+            j++;
+        }
+        System.out.println("Merge Two Sort Array "+Arrays.toString(mergeArray));
+
+    }
+    public void mergeSort(int[] arr){
+
+    }
     public static void main(String[] args) {
         int[] arr ={2,1,4,9,0,5,6,3};
         bubbleSort(arr);
         selectionSort(arr);
         insertionSort(arr);
+        int[] arr1={1,3,5,8};
+        int[] arr2={2,4,6,9};
+        mergeTwoSortArray(arr1,arr2);
+
     }
 }
